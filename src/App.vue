002e6dea41f4f1d7415ce68222deddbecc6b8a0b
V02008 <354294215@qq.com>
2019-03-22 18:47:38 +0800

    github开源

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,112 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app-phone">
+      <div class="phone-header">
+        <a 
+          class="cancel-cta"
+          v-if="step===2||step===3"
+          @click="goToHome"
+          >
+          Cancel
+        </a>
+        <img src="./assets/vue_gram_logo_cp.png" >
+        <a 
+          class="next-cta"
+           v-if="step===2"
+           @click="step++">
+          Next
+        </a>
+        <a 
+          class="next-cta"
+           v-if="step===3"
+           @click="share"
+           >
+          Share
+        </a>
+      </div>
+      <PhoneBody 
+        :posts="posts"
+        :step="step"
+        :image="image"
+        :filters="filters"
+        :selectFilter="selectFilter"
+        v-model="caption"
+      ></PhoneBody>
+      <div class="phone-footer">
+        <div class="home-cta">
+          <i class="fas fa-home fa-lg" @click="goToHome"></i>
+        </div>
+        <div class="upload-cta">
+          <input type="file" id="file" name="file" @change="uploadImage" :disabled="step !==1">
+          <label for="file">
+            <i class="fas fa-plus-square fa-lg"></i>
+          </label>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PhoneBody from "./components/PhoneBody.vue"
+import posts from "./data/posts.js"
+import filters from "./data/filters.js"
+import eventBus from "./event-bus.js"
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  created(){
+    eventBus.$on("selectedFilter", event=>{
+      this.selectFilter=event;
+    })
+  },
+  data(){
+    return{
+      posts,
+      step:1,
+      image:"",
+      filters,
+      selectFilter:"",
+      caption:""
+    }
+  },
+  components:{
+    PhoneBody
+  },
+  methods:{
+    uploadImage(event){
+      const files = event.target.files;
+      if(!files.length) return;
+
+      const reader = new FileReader();
+      reader.readAsDataURL(files[0]);
+      reader.onload = event =>{
+        this.image =event.target.result;
+        this.step=2;
+      }
+    },
+    goToHome(){
+      this.image="";
+      this.selectFilter="";
+      this.step =1;
+    },
+    share(){
+      const post = {
+        username: "赵旺",
+        userImage: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/me_3.jpg",
+        postImage:this.image,
+        likes: 0,
+        hasBeenLiked: false,
+        caption: this.caption,
+        filter: this.selectFilter
+      }
+      this.posts.unshift(post);
+      this.goToHome();
+    }
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" src="./styles/app.scss">
+
+
 </style>
---
+++ src/components/VuegramPost.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="vuegram-post">
+    <div class="header lever">
+      <div class="lever-left">
+        <figure class="image is-32x32">
+          <img :src="post.userImage">
+        </figure>
+        <span class="uesrname">{{post.username}}</span>
+      </div>
+    </div>
+    <div
+      class="image-container"
+      :class="post.filter"
+      @dblclick="clicklike"
+      :style="{backgroundImage:'url('+post.postImage+')'}"
+    ></div>
+    <div class="content">
+      <div class="heart">
+        <i class="far fa-heart"
+            @click="clicklike"
+            :class="{fas : this.post.hasBeenLiked}"
+        ></i>
+      </div>
+      <p class="likes">{{post.likes}}likes</p>
+      <p class="caption">
+        <span>{{post.username}}</span>
+        {{post.caption}}
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    props:{
+        post:Object
+    },
+    methods:{
+        clicklike(){
+            this.post.hasBeenLiked ? this.post.likes-- : this.post.likes++
+            this.post.hasBeenLiked = !this.post.hasBeenLiked
+        }
+    }
+};
+</script>
+<style lang="scss" src="../styles/vuegram-post.scss">
+</style>
+
+
